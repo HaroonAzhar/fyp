174b92fac446ecb571aa48de21207268a0929f13
Haroon Azhar Khan <haroon1azhar@gmail.com>
2021-04-12 14:47:40 +0500

    fyp-initial-commit

---
+++ server/models/shop.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const ShopSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Name is required",
+  },
+  image: {
+    data: Buffer,
+    contentType: String,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  number_of_sales: {
+    type: Number,
+  },
+  is_a_weak_seller: {
+    type: Boolean,
+  },
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  owner: { type: mongoose.Schema.ObjectId, ref: "User" },
+});
+
+export default mongoose.model("Shop", ShopSchema);
