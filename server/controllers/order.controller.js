a6075e4340f8009c803ab735c4ec4ee9455a3338
Haroon Azhar Khan <haroon1azhar@gmail.com>
2021-04-13 11:27:17 +0500

    order are aable to be taken without  stripes

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -1,82 +1,91 @@
-import {Order, CartItem} from '../models/order.model'
-import errorHandler from './../helpers/dbErrorHandler'
+import { Order, CartItem } from "../models/order.model";
+import errorHandler from "./../helpers/dbErrorHandler";
 
 const create = async (req, res) => {
+  console.log("req.profile", req.body.order.user);
+  console.log("order", req.body.order);
+
   try {
-    req.body.order.user = req.profile
-    let order = new Order(req.body.order)
-    let result = await order.save()
-    res.status(200).json(result)
-  } catch (err){
+    req.body.order.user = req.profile;
+    let order = new Order(req.body.order);
+    let result = await order.save();
+    res.status(200).json(result);
+  } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 
 const listByShop = async (req, res) => {
   try {
-    let orders = await Order.find({"products.shop": req.shop._id})
-      .populate({path: 'products.product', select: '_id name price'})
-      .sort('-created')
-      .exec()
-    res.json(orders)
-  } catch (err){
+    let orders = await Order.find({ "products.shop": req.shop._id })
+      .populate({ path: "products.product", select: "_id name price" })
+      .sort("-created")
+      .exec();
+    res.json(orders);
+  } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 
 const update = async (req, res) => {
   try {
-    let order = await Order.updateOne({'products._id':req.body.cartItemId}, {
-        'products.$.status': req.body.status
-    })
-      res.json(order)
-  } catch (err){
+    let order = await Order.updateOne(
+      { "products._id": req.body.cartItemId },
+      {
+        "products.$.status": req.body.status,
+      }
+    );
+    res.json(order);
+  } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 
 const getStatusValues = (req, res) => {
-  res.json(CartItem.schema.path('status').enumValues)
-}
+  res.json(CartItem.schema.path("status").enumValues);
+};
 
 const orderByID = async (req, res, next, id) => {
   try {
-    let order = await Order.findById(id).populate('products.product', 'name price').populate('products.shop', 'name').exec()
+    let order = await Order.findById(id)
+      .populate("products.product", "name price")
+      .populate("products.shop", "name")
+      .exec();
     if (!order)
-      return res.status('400').json({
-        error: "Order not found"
-      })
-    req.order = order
-    next()
-  } catch (err){
+      return res.status("400").json({
+        error: "Order not found",
+      });
+    req.order = order;
+    next();
+  } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 
 const listByUser = async (req, res) => {
-  try{
-    let orders = await Order.find({ "user": req.profile._id })
-        .sort('-created')
-        .exec()
-    res.json(orders)
-  } catch (err){
+  try {
+    let orders = await Order.find({ user: req.profile._id })
+      .sort("-created")
+      .exec();
+    res.json(orders);
+  } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 
 const read = (req, res) => {
-  return res.json(req.order)
-}
+  return res.json(req.order);
+};
 
 export default {
   create,
@@ -85,5 +94,5 @@ export default {
   getStatusValues,
   orderByID,
   listByUser,
-  read
-}
+  read,
+};
