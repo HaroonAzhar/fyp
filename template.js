e9fc166b4e8d591725891776d29ef60cc11e5a78
Haroon Azhar Khan <haroon1azhar@gmail.com>
2021-05-07 13:28:00 +0500

    heroku chnaged  app

--- template.js
+++ template.js
@@ -1,5 +1,5 @@
-export default ({markup, css}) => {
-    return `<!doctype html>
+export default ({ markup, css }) => {
+  return `<!doctype html>
       <html lang="en">
         <head>
           <meta charset="utf-8">
@@ -13,10 +13,34 @@ export default ({markup, css}) => {
           </style>
         </head>
         <body style="margin:0">
+        <!-- Messenger Chat plugin Code -->
+        <div id="fb-root"></div>
+          <script>
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v10.0'
+              });
+            };
+    
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          </script>
+    
+          <!-- Your Chat plugin code -->
+          <div class="fb-customerchat"
+            attribution="page_inbox"
+            page_id="100268701577391">
+          </div>
           <div id="root">${markup}</div>
           <style id="jss-server-side">${css}</style>
           <script id="stripe-js" src="https://js.stripe.com/v3/" async></script>
           <script type="text/javascript" src="/dist/bundle.js"></script>
         </body>
-      </html>`
-}
+      </html>`;
+};
